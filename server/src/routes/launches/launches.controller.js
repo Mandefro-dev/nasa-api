4f64d976e66afd40853e3aa3a0db1b0327bd66e8
Mandefro Endalk <mandefroendalk00@gmail.com>
2026-02-01 22:59:04 +0300

    fizinf erro hadnling in launch controller

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -23,51 +23,60 @@ async function httpGetAllLaunches(req, res) {
 
 async function httpAddNewLaunch(req, res) {
   let launch = req.body;
-  if (
-    !launch.mission ||
-    !launch.rocket ||
-    !launch.launchDate ||
-    !launch.destination
-  ) {
-    return res.status(400).json({
-      error: "missing required launch property",
-    });
+  try {
+    if (
+      !launch.mission ||
+      !launch.rocket ||
+      !launch.launchDate ||
+      !launch.destination
+    ) {
+      return res.status(400).json({
+        error: "missing required launch property",
+      });
+    }
+
+    launch.launchDate = new Date(launch.launchDate);
+    //if launch data is correct date formt isNan(date) would be false.
+
+    //but if it valid date,,, isNan do this, it converts the date to a number, using this  date.valuesOf()
+    if (isNaN(launch.launchDate)) {
+      return res.status(400).json({
+        error: "Invalid launch Date",
+      });
+    }
+
+    await scheduleNewLaunch(launch);
+    console.log(launch);
+    return res.status(201).json(launch);
+  } catch (error) {
+    return res.status(400).json(error.message);
   }
-
-  launch.launchDate = new Date(launch.launchDate);
-  //if launch data is correct date formt isNan(date) would be false.
-
-  //but if it valid date,,, isNan do this, it converts the date to a number, using this  date.valuesOf()
-  if (isNaN(launch.launchDate)) {
-    return res.status(400).json({
-      error: "Invalid launch Date",
-    });
-  }
-
-  await scheduleNewLaunch(launch);
-  console.log(launch);
-  return res.status(201).json(launch);
 }
 async function httpAbortLaunch(req, res) {
   const launchId = +req.params.id;
-  const existsLaunch = await existsWithLaunchId(launchId);
-  if (!existsLaunch) {
-    return res.status(404).json({
-      error: "launch not found",
+
+  try {
+    const existsLaunch = await existsWithLaunchId(launchId);
+    if (!existsLaunch) {
+      return res.status(404).json({
+        error: "launch not found",
+      });
+    }
+
+    const aborted = await abortLaunchById(launchId);
+
+    if (!aborted) {
+      return res.status(400).json({
+        error: "Launch not aborted",
+      });
+    }
+
+    return res.status(200).json({
+      ok: true,
     });
+  } catch (error) {
+    return res.status(400).json(error.message);
   }
-
-  const aborted = await abortLaunchById(launchId);
-
-  if (!aborted) {
-    return res.status(400).json({
-      error: "Launch not aborted",
-    });
-  }
-
-  return res.status(200).json({
-    ok: true,
-  });
 }
 
 module.exports = {
